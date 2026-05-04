9aed1dbe76f38aa8e19328bad1c39a01317232a9
ab020206 <arihantpvt02@gmail.com>
2026-05-04 21:04:03 +0530

    --

--- without-cdn/W10/server/server.js
+++ without-cdn/W10/server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../')));
 
